1431bafd2f761f5af60e259972bef72f25a56c6a
duyhien2410 <hienduy2410@gmail.com>
2022-05-17 08:40:28 +0700

    up bai 9

--- ss9_aop/bai_tap/book_rental_app/src/main/java/com/codegym/controller/BookController.java
+++ ss9_aop/bai_tap/book_rental_app/src/main/java/com/codegym/controller/BookController.java
@@ -11,10 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class BookController {
@@ -93,6 +90,11 @@ public class BookController {
         return "redirect:/list";
 
     }
+
+//    @ExceptionHandler(NullPointerException.class)
+//    public String showErrorPage(){
+//        return "error";
+//    }
 
     private String randomBorrowedBook() {
         String result = "";
